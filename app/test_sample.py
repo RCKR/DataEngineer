ae26076af2f25023d1d4eb89c48e8180da68a688
RCKR <richard.raman@saunalahti.fi>
2023-10-07 20:08:54 +0200

    adding missing link

--- app/test_sample.py
+++ app/test_sample.py
@@ -3,6 +3,7 @@ import sample
 
 # Documentation for the following script:
 # The idea is to test different functions.
+# this is great as well...
 
 def test_return_fun_text():
     # this is a comment about the code.
@@ -12,3 +13,8 @@ def test_return_fun_text():
 def test_multiply_objects_by_five():
     assert sample.multiply_objects_by_five(3) == 15
     assert sample.multiply_objects_by_five(5) == 25
+
+
+def test_find_the_missing_link():
+    assert sample.find_the_missing_link('main') == True
+    assert sample.find_the_missing_link('not main') == False
